32583885558a514d79a8708f3cad5c674d898002
joepiuls <john71688@gmail.com>
2025-02-13 20:31:52 +0100

    4th commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,10 +26,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messagesRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../../frontend')));
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../../frontend/dist', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
   })
 }
 
